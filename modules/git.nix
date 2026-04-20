85cc6b006ddb8a71fdecc80c99615759ebc342f0
Matthew deHaas <mattdehaas28@gmail.com>
2026-04-20 00:35:58 -0700

    Attempt to silence warnings.

--- modules/git.nix
+++ modules/git.nix
@@ -8,6 +8,11 @@
 				name = "Matthew deHaas";
 				email = "mattdehaas28@gmail.com";
 			};
+			signing = {
+				key = "AEA7178A37D0A93B";
+				signByDefault = true;
+				format = "openpgp";
+			};
 
 			init.defaultBranch = "main";
 			push.autoSetupRemote = true;
