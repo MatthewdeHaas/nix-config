387ae6f15f9d65a9b75a3c6d2daeb5c7a103ed92
Matthew deHaas <mattdehaas28@gmail.com>
2026-04-19 20:47:17 -0700

    First commit,

---
+++ flake.nix
@@ -0,0 +1,27 @@
+# Tells nix to fetch Nixpkgs and Home Manager Libraries
+{
+	description: "Matt's Universal Nix Configuration file";
+
+	inupts = {
+		# Where packages are retrieved from
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+		# Home manager
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+
+	outputs = {
+		let 
+			system = "aarch64-darwin";
+			pkgs = nixpkgs.legacyPackages.${system};
+		in {
+			homeConfigurations."matt" = home-manager.lib.homeManagerConfiguration {
+				inherit pkgs;
+				modules = [ .modules/home.nix ]
+			};
+		};
+	};
+}
---
+++ modules/home.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... };
+
+{
+	home.username = "matt";
+	home.homeDirectory = "/User/matthewdehaas";
+	home.stateVersion = "23.11";
+
+	# Packages
+	home.packages = with pkgs; [
+		direnv
+		nix-direnv
+	]
+
+	programs.zsh = {
+		enable = true;
+		shellAlias = {
+			nixswitch = "home-manager switch --flake ~/nix-config#matt";
+		};
+	};
+
+	# Let Home Manager install and manage itself 
+	programs.home-manager.enable = true;
+}
