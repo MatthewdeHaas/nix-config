b2b9ebb25e84868468a6f64f60bd8e8b993b854e
Matthew deHaas <mattdehaas28@gmail.com>
2026-04-20 00:31:55 -0700

    Remove warning from switch call.

--- modules/home.nix
+++ modules/home.nix
@@ -68,7 +68,7 @@
 	services.gpg-agent = {
 		enable = true;
 		defaultCacheTtl = 1800;
-		pinentryPackage = pkgs.pinentry_mac;
+		pinentry.package = pkgs.pinentry_mac;
 		extraConfig = ''
 			pinentry-program ${pkgs.pinentry_mac}/Applications/pinentry-mac.app/Contents/MacOS/pinentry-mac
 		'';
