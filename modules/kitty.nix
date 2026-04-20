95e6a81159751d906a96e8bdc348f9ec61306964
Matthew deHaas <matthewdehaas@dhcp-206-87-159-208.ubcsecure.wireless.ubc.ca>
2026-04-19 21:59:02 -0700

    Use new font import.

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -16,37 +16,36 @@
 
 		# Settings
 		settings = {
-      # Window & Padding
-      window_padding_width = 10;
-      hide_window_decorations = "yes";
+			# Window & Padding
+			window_padding_width = 10;
+			hide_window_decorations = "yes";
 
-      # Tab Bar
-      tab_bar_style = "powerline";
-      tab_powerline_style = "slanted";
-      active_tab_foreground = "#11111b";
-      active_tab_background = "#cba6f7";
-      inactive_tab_foreground = "#cdd6f4";
-      inactive_tab_background = "#181825";
+			# Tab Bar
+			tab_bar_style = "powerline";
+			tab_powerline_style = "slanted";
+			active_tab_foreground = "#11111b";
+			active_tab_background = "#cba6f7";
+			inactive_tab_foreground = "#cdd6f4";
+			inactive_tab_background = "#181825";
 
-      # Cursor
-      cursor_shape = "beam";
-      cursor_beam_thickness = "1.5";
+			# Cursor
+			cursor_shape = "beam";
+			cursor_beam_thickness = "1.5";
 
-      # Mouse & Interaction
-      focus_follows_mouse = "yes";
-      detect_urls = "yes";
-    };
+			# Mouse & Interaction
+			focus_follows_mouse = "yes";
+			detect_urls = "yes";
+		};
 
-    # Misc. 
-    extraConfig = ''
-      # Allow scrolling to send actual up/down keys
-      mouse_reporting yes
-    '';
-	};
+		# Misc. 
+		extraConfig = ''
+			# Allow scrolling to send actual up/down keys
+			mouse_reporting yes
+		'';
+		};
 
 	# Ensure font is available
 	home.packages = [
-		(pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+		pkgs.nerd-fonts.jetbrains-mono # This is the modern, faster way to do it
 	];
-
 }
