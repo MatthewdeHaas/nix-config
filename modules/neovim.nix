8277e7cdf45554436b5eef7c555221b4299b34d7
Matthew deHaas <mattdehaas28@gmail.com>
2026-04-22 18:37:12 -0700

    Update latex config.

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -88,13 +88,13 @@
 			-- VimTeX Config
 			vim.g.vimtex_view_method = "skim"
 			vim.g.vimtex_compiler_method = "latexmk"
-			vim.g.vimtex_compiler_latexmk_engines = { _ = "xelatex" }
-			vim.g.vimtex_build_dir = ".build"
 			vim.g.vimtex_quickfix_mode = 0
+			vim.g.vimtex_build_dir = ".build"
 			vim.g.vimtex_compiler_latexmk = {
+				executable = "latexmk",
 				out_dir = ".build",
 				options = {
-					"-xelatex",
+					"-pdfxe",
 					"-shell-escape",
 					"-synctex=1",
 					"-interaction=nonstopmode",
