d076cd093cc01c46890462a93b233eacef9a0d6a
Matthew deHaas <mattdehaas28@gmail.com>
2026-04-23 18:36:58 -0700

    Sync 2026-04-23 18:36

--- modules/snippets/tex.lua
+++ modules/snippets/tex.lua
@@ -1,3 +1,10 @@
-s({trig = "mk", wordTrig = true, snippetType = "autosnippet"},
-  fmt("${}$", { i(1) })
-),
+local ls = require("luasnip")
+local s = ls.snippet
+local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+return {
+  s({trig = "mk", wordTrig = true, snippetType = "autosnippet"},
+    fmt("${}$", { i(1) })
+  ),
+}
