c18e6511a97b1123305b638610eb2ac617aa9cbe
Matthew deHaas <mattdehaas28@gmail.com>
2026-05-18 17:40:57 -0700

    Sync 2026-05-18 17:40

--- modules/jj.nix
+++ modules/jj.nix
@@ -1,24 +1,26 @@
 { pkgs, ... }:
 
+let
+  jjConfig = pkgs.writeText "jj-config.toml" ''
+    [user]
+    name = "Matthew"
+    email = "mattdehaas28@gmail.com"
+
+    [ui]
+    paginate = "never"
+    default-command = "log"
+    log-template = 'separate(" ", change_id.shortest(), if(empty, "(empty)"), if(conflict, "(conflict)"), description.first_line()) ++ "\n"'
+
+    [aliases]
+    l = ["log", "--no-pager"]
+  '';
+in
 {
   home.packages = [ pkgs.jujutsu ];
 
-	xdg.configFile."jj/config.toml".text = ''
-		[user]
-		name = "Matthew"
-		email = "mattdehaas28@gmail.com"
-
-		[ui]
-		paginate = "never"
-		default-command = "log"
-		log-template = 'separate(" ", if(current_working_copy, "@", if(immutable, "◆", "○")), change_id.shortest(), if(empty, "(empty)"), if(conflict, "(conflict)"), description.first_line()) ++ "\n"'
-
-		[aliases]
-		l = ["log", "--no-pager"]
-	'';
+  xdg.configFile."jj/config.toml".source = jjConfig;
 
   programs.zsh.initExtra = ''
-    # jj completions
     source <(COMPLETE=zsh jj)
   '';
 }
