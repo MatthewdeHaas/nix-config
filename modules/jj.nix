bf8bcea875693865f96510f71ca16ff08df96251
Matthew deHaas <mattdehaas28@gmail.com>
2026-05-18 17:33:54 -0700

    Sync 2026-05-18 17:33

--- modules/jj.nix
+++ modules/jj.nix
@@ -3,19 +3,19 @@
 {
   home.packages = [ pkgs.jujutsu ];
 
-	xdg.configFile."jj/config.toml".text = ''
-		[user]
-		name = "Matthew"
-		email = "mattdehaas28@gmail.com"
-
-		[ui]
-		paginate = "never"
-		default-command = "log"
-		log-template = "separate(\" \", if(current_working_copy, \"@\", if(immutable, \"◆\", \"○\")), change_id.shortest(), if(empty, \"(empty)\"), if(conflict, \"(conflict)\"), description.first_line()) ++ \"\\n\""
-
-		[aliases]
-		l = ["log", "--no-pager"]
-'';
+	xdg.configFile."jj/config.toml".text = builtins.concatStringsSep "\n" [
+		"[user]"
+		"name = \"Matthew\""
+		"email = \"mattdehaas28@gmail.com\""
+		""
+		"[ui]"
+		"paginate = \"never\""
+		"default-command = \"log\""
+		''log-template = 'separate(" ", if(current_working_copy, "@", if(immutable, "◆", "○")), change_id.shortest(), if(empty, "(empty)"), if(conflict, "(conflict)"), description.first_line()) ++ "\n"' ''
+		""
+		"[aliases]"
+		''l = ["log", "--no-pager"]''
+	];
 
   programs.zsh.initExtra = ''
     # jj completions
